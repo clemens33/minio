8496c50b2fd973dd06b799a73cba484b13fee706
clemens33 <yongwu33@gmail.com>
2021-04-11 18:05:59 +0200

    fixes

--- start.sh
+++ start.sh
@@ -8,18 +8,18 @@ export MINIO_CONSOLE_PORT=9091
 export MINIO_ROOT_USER=minio
 export MINIO_ROOT_PASSWORD=minio123
 
-ln -s /data/minio ./data
+ln -s $MINIO_STORAGE_LOCATION ./data
 
 docker-compose pull
 docker-compose up --remove-orphans -d
 
-alias mc='docker exec -it minio-client mc'
+mc='docker exec -it minio-client mc'
 
 # create console user and policy
 export MINIO_CONSOLE_USER=console
 export MINIO_CONSOLE_PASSWORD=console123
-mc admin user add minio/ $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
-mc admin policy add minio/ consoleAdmin /root/.mc/admin.json
-mc admin policy set minio consoleAdmin user=$MINIO_CONSOLE_USER
+$mc admin user add minio/ $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
+$mc admin policy add minio/ consoleAdmin /root/.mc/admin.json
+$mc admin policy set minio consoleAdmin user=$MINIO_CONSOLE_USER
 
 
